0bf31cc3af664f9766a638e1fd77580786f45aed
Brandonv02 <brandonvidesd@gmail.com>
2023-08-19 13:24:53 -0500

    init
    commit init

---
+++ lib/app/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'config/themes/theme.dart';
+import 'presentation/views/views_links.dart';
+
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme(selectedThemeColor: 0).theme(),
+      home: const LoginView(),
+    );
+  }
+}
